44f3180489bad42eac49285d249ae085583d3267
Zehra <sana.zehra@accenture.com>
2025-05-29 12:55:48 +0530

    Patient management

--- Fastapi_tutorials/main.py
+++ Fastapi_tutorials/main.py
@@ -1,11 +1,23 @@
+# to run the code : uvicorn main:app --reload 
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+    return data
+
 @app.get('/')
 def hello():
-    return {'message':"Hello"}
+    return {'message':"Patient Management System API"}
 
 @app.get('/about')
 def about():
-    return {'message' : 'AI is my favourite topic to explore'}
+    return {'message' : 'A fully functional API to manage your patient records'}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
