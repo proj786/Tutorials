e156a5e8915036cc3d9d66c0253fb6e6b611939f
sana.zehra <sana.zehra@accenture.com>
2025-08-10 07:12:43 +0530

    Updated

---
+++ Langchain_models/2.ChatModels/4_chatmodel_hf_api.py
@@ -0,0 +1,13 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id = "mistralai/Mistral-7B-Instruct-v0.3",
+    task = "text-generation"
+
+)
+model = ChatHuggingFace(llm = llm )
+result = model.invoke("What is capital of India")
+print(result.content)
