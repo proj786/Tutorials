64b1e8eb4887fd7e7e81c0ca45ff7cdb022eb234
sana.zehra <sana.zehra@accenture.com>
2025-08-11 11:57:33 +0530

    Updated codes

--- Langchain/Langchain_prompts/prompt_ui.py
+++ Langchain/Langchain_prompts/prompt_ui.py
@@ -1,2 +1,12 @@
 from langchain_openai import chatopenAI
-from dotenv import
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+st.header('Research Tool')
+
+user_input =st.text_input('Enter your prompt')
+
+if st.button:
+    st.text('Some random text')
